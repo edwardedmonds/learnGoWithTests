bd8650de31b094074e7c73a9c1a7abf706ad0be5
Edward Edmonds <edward.edmonds@gmail.com>
2023-09-03 22:00:24 -0400

    bleh

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+)
+
 // func Sum(numbers [5]int) int {
 // 	sum := 0
 // 	for i := 0; i < 5; i++ {
@@ -14,4 +18,23 @@ func Sum(numbers []int) int {
 		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+func main() {
+	sliceOne := []int{12, 23, 3, 4, 5}
+	sliceTwo := []int{3, 5}
+	sliceThree := []int{20, 39, 38}
+
+	fmt.Println(SumAll(sliceOne, sliceTwo, sliceThree))
 }
