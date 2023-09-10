fd4ef50ef3ee6b80a380fc964c810cb48a74d7a8
Edward Edmonds <edward.edmonds@gmail.com>
2023-09-10 12:43:48 -0400

    add test for insufficient funds

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,38 +1,41 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	t.Run("deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
 		got := wallet.Balance()
-
-		fmt.Printf("address of balance in test is %v \n", &wallet.balance)
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
 		wallet.Withdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("wanted an error but didn't get one")
 		}
 	})
 }
